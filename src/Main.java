78c5483c3234a45949a4f6dbd36a295fd171d72d
muhammed.bilal <bilal.abdulkany@gmail.com>
2023-05-16 10:11:03 +0400

    caesarcipher

--- src/Main.java
+++ src/Main.java
@@ -14,14 +14,14 @@ public class Main {
 
         List<Integer> sortedArray = arr.stream().sorted().collect(Collectors.toList());
 
-        long sum=0,maxSum=0,minSum=0;
-        for (int i=0;i<arr.size(); i++) {
-            sum+=arr.get(i);
+        long sum = 0, maxSum = 0, minSum = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            sum += arr.get(i);
         }
-        minSum=sum - arr.get(arr.size()).longValue();
-        maxSum = sum-arr.get(0).longValue();
+        minSum = sum - arr.get(arr.size()).longValue();
+        maxSum = sum - arr.get(0).longValue();
 
-        System.out.println(minSum + " "+ maxSum);
+        System.out.println(minSum + " " + maxSum);
     }
 
 
@@ -29,7 +29,7 @@ public class Main {
         // Write your code here
 
         DateFormat format = new SimpleDateFormat("hh:mm:ssaa");
-        var inputDate= format.parse(s);
+        var inputDate = format.parse(s);
         DateFormat output = new SimpleDateFormat("HH:mm:ss");
         String date = output.format(inputDate);
         return date;
@@ -40,55 +40,57 @@ public class Main {
         List<Integer> sorted = arr.stream().sorted().collect(Collectors.toList());
         int size = sorted.size();
 
-        int median = size/2;
+        int median = size / 2;
 
         return sorted.get(median);
 
     }
+
     public static int lonelyinteger(List<Integer> a) {
         // Write your code here
-        Map<Integer,Integer> uniqueSet= new HashMap<Integer,Integer>();
+        Map<Integer, Integer> uniqueSet = new HashMap<Integer, Integer>();
 
         a = a.stream().sorted().collect(Collectors.toList());
-        int index=0;
-        for(int i=0; i<a.size();i++){
-            if(uniqueSet.get(a.get(i))== null)
-                uniqueSet.put(a.get(i),1);
-                else
-                    uniqueSet.remove(a.get(i));
+        int index = 0;
+        for (int i = 0; i < a.size(); i++) {
+            if (uniqueSet.get(a.get(i)) == null)
+                uniqueSet.put(a.get(i), 1);
+            else
+                uniqueSet.remove(a.get(i));
         }
 
-        uniqueSet.entrySet().stream().forEach(c ->      System.out.println(c));
+        uniqueSet.entrySet().stream().forEach(c -> System.out.println(c));
         return uniqueSet.entrySet().stream().mapToInt(v -> v.getKey()).findFirst().getAsInt();
     }
 
     public static int diagonalDifference(List<List<Integer>> arr) {
         // Write your code here
-        int firstDiag=0, secondDiag=0;
-        for (int i=0; i<arr.size();i++){
-            for(int k=0;k<arr.get(i).size();k++){
-                if(i == k){
-                    firstDiag +=arr.get(i).get(k);
+        int firstDiag = 0, secondDiag = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            for (int k = 0; k < arr.get(i).size(); k++) {
+                if (i == k) {
+                    firstDiag += arr.get(i).get(k);
                 }
-                if(i+k == arr.get(i).size()-1){
+                if (i + k == arr.get(i).size() - 1) {
                     secondDiag += arr.get(i).get(k);
                 }
             }
         }
-        int diagVal= firstDiag-secondDiag;
+        int diagVal = firstDiag - secondDiag;
         return Math.abs(diagVal);
     }
+
     public static List<Integer> countingSort(List<Integer> arr) {
-        var fillArray =IntStream.generate(()->0).limit(101).toArray();
-        int index=0;
-        for(int i=0; i<arr.size(); i++){
-            int arrayElement = arr.get(i);// 1 2 2 3 3 3
-            index= fillArray[arrayElement];
-            fillArray[arr.get(i)]= ++index;
+        var fillArray = IntStream.generate(() -> 0).limit(101).toArray();
+        int index = 0;
+        // 1 2 2 3 3 3
+        for (int arrayElement : arr) {
+            index = fillArray[arrayElement];
+            fillArray[arrayElement] = ++index;
 
         }
 
-        Arrays.stream(fillArray).forEach(c-> System.out.print(c + " "));
+        Arrays.stream(fillArray).forEach(c -> System.out.print(c + " "));
 
 
         return Arrays.stream(fillArray).boxed().collect(Collectors.toList());
@@ -96,8 +98,8 @@ public class Main {
     }
 
     public static int flippingMatrix(List<List<Integer>> matrix) {
-        int sum=0;
-        int arrSize=matrix.size();
+        int sum = 0;
+        int arrSize = matrix.size();
 
         //{ [112, 42, 114, 119] ,
         // [56, 125, 101, 49] ,
@@ -121,59 +123,91 @@ public class Main {
         // max of [N-1,N-1] -> [0,N-1], [N-1,0], [0,0]
         //max of [r,c] -> [r,c], [N-r-1,c],[r,N-c-1],  [N-r-1, N-c-1]
 
-        matrix.stream().forEach(c -> System.out.println(c));
+        matrix.forEach(System.out::println);
 
-        for(int r=0; r<arrSize/2;r++ ){
-            for(int c=0;c<arrSize/2; c++){
-                sum+= Math.max(Math.max( Math.max(matrix.get(r).get(c),matrix.get(r).get(arrSize-c-1)),matrix.get(arrSize-r-1).get(c)),
-                        matrix.get(arrSize-r-1).get(arrSize-c-1));
+        for (int r = 0; r < arrSize / 2; r++) {
+            for (int c = 0; c < arrSize / 2; c++) {
+                sum += Math.max(Math.max(Math.max(matrix.get(r).get(c), matrix.get(r).get(arrSize - c - 1)), matrix.get(arrSize - r - 1).get(c)),
+                        matrix.get(arrSize - r - 1).get(arrSize - c - 1));
             }
         }
 
         return sum;
     }
 
-    public static void findZigZagSequence(int [] a, int n){
+    public static void findZigZagSequence(int[] a, int n) {
 
         Arrays.sort(a);
-        int mid = (n+1)/2-1;
+        int mid = (n + 1) / 2 - 1;
         int temp = a[mid];
         a[mid] = a[n - 1];
         a[n - 1] = temp;
         int st = mid + 1;
         int ed = n - 2;
 
-      while(st <= ed){
+        while (st <= ed) {
 
-          if(a[st] <= a[ed]) {
-              temp = a[ed];
-              a[ed] = a[st];
-              a[st] = temp;
-          }
+            if (a[st] <= a[ed]) {
+                temp = a[ed];
+                a[ed] = a[st];
+                a[st] = temp;
+            }
             st = st + 1;
             ed = ed - 1;
         }
         System.out.println();
-        for(int i = 0; i < n; i++){
-            if(i > 0) System.out.print(" ");
+        for (int i = 0; i < n; i++) {
+            if (i > 0) System.out.print(" ");
             System.out.print(a[i]);
         }
     }
 
     public static int towerBreakers(int n, int m) {
-        return (n%2==0 || m==1 )?2:1;
+        return (n % 2 == 0 || m == 1) ? 2 : 1;
     }
 
     public static String caesarCipher(String s, int k) {
-        // Write your code here
-    return "";
+    //fff.jkl.gh
+        char[] charArray = s.toCharArray();
+        char singleChar;
+        int modChar = 0;
+        StringBuffer output = new StringBuffer();
+
+
+        for (int i = 0; i < charArray.length; i++) {
+
+            if (Character.isAlphabetic(charArray[i])) {
+
+                singleChar = Character.toLowerCase(charArray[i]);
+
+                k=k%26;
+
+                System.out.println("k%26: "+ k);
+                modChar = singleChar + k;
+                if(modChar>122){
+                    modChar=96+(modChar-122);
+                }
+
+
+
+                singleChar = (char) (modChar);
+
+
+                singleChar = Character.isUpperCase(charArray[i]) ? Character.toUpperCase(singleChar) : singleChar;
+                output.append(singleChar);
+            } else {
+                output.append(charArray[i]);
+            }
+        }
+
+        return output.toString();
     }
 
 
     public static void main(String[] args) throws ParseException {
 
         List<List<Integer>> sqMatrix = new ArrayList<>();
-            List<Integer> input = new ArrayList<>();
+        List<Integer> input = new ArrayList<>();
 
         input.add(112);
         input.add(42);
@@ -205,10 +239,16 @@ public class Main {
         input4.add(108);
         sqMatrix.add(input4);
 
-        int [] a = new int[7];
-        a[0]=1; a[1]=2;a[2]= 3;a[3]=4; a[4]=5;a[5]=6;a[6]=7;
+        int[] a = new int[7];
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+        a[3] = 4;
+        a[4] = 5;
+        a[5] = 6;
+        a[6] = 7;
 
-        System.out.println(towerBreakers(2,6));
+        System.out.println(caesarCipher("www.abc.xyza123", 87));
 
     }
 }
