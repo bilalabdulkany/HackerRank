bc0a188f210d7ba3cf5418172f3a2bd0fa766e8a
muhammed.bilal <bilal.abdulkany@gmail.com>
2023-05-23 14:30:47 +0400

    mergeLists - SingleLinkedList

--- src/Main.java
+++ src/Main.java
@@ -396,7 +396,55 @@ public class Main {
 
         return start;
     }
+   private static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
 
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    private static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+    static SinglyLinkedListNode mergeLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+
+        SinglyLinkedList sortedList = new SinglyLinkedList();
+
+        return  sortedList.head;
+    }
 
     public static void main(String[] args) throws IOException {
         List<Integer> m = new ArrayList<>();
